becae8d07586be2a43400219418246cbbb26596b
Anaïs <aekyon.08@gmail.com>
2023-11-20 18:53:22 +0100

    Create createCard function

--- script.js
+++ script.js
@@ -110,83 +110,14 @@ inputButton.addEventListener('click', async function() {
     const container = document.querySelector(".container");
 
     const data = await fetchApiData(cityName);
-    clearContainer();
+
             
             cityNameContainer.textContent = data.location.name + ", " + data.location.country;
-
-            for(let i= 0; i < 5; i++) {
-                const container = document.querySelector('.container');
-
-                const date = new Date()
-                const dayOfTheWeek = weekdays[(date.getDay() + i) % 7]
-            
-                // Create the elements with Data
-                const card = document.createElement('div');
-                card.classList.add("card");
-            
-                if (i === 0) card.classList.add("main-card");
-            
-                container.appendChild(card);
-            
-                const imageBox = document.createElement('div');
-                imageBox.classList.add("imgBx");
-                card.appendChild(imageBox);
-            
-                const cardImg = document.createElement('img');
-                cardImg.src = data.forecast.forecastday[i].day.condition.icon;
-                imageBox.appendChild(cardImg);
-                
-                const contentBox = document.createElement("div");
-                contentBox.classList.add("contentBx");
-                card.appendChild(contentBox);
-            
-                const cardHeader = document.createElement("h2");
-                cardHeader.innerHTML = dayOfTheWeek;
-                contentBox.appendChild(cardHeader);
-            
-                console.log(data.forecast.forecastday[i].day.condition.text);
-                const tempDescription = document.createElement("h4");
-                tempDescription.innerHTML = data.forecast.forecastday[i].day.condition.text;
-                contentBox.appendChild(tempDescription);
-            
-                const currentTempBox = document.createElement("div");
-                currentTempBox.classList.add("color");
-                contentBox.appendChild(currentTempBox)
-            
-                const currentTempHeader = document.createElement("h3");
-                currentTempHeader.innerHTML = "Temp:"
-                currentTempBox.appendChild(currentTempHeader);
-            
-                const currentTemp = document.createElement("span");
-                currentTemp.classList.add("current-temp");
-
-                // OLD structure from different API
-                // let averageTemp = (result.daily.temperature_2m_min[i] + result.daily.temperature_2m_max[i]) / 2;
-                // if(i === 0) averageTemp = result.current.temperature_2m;
-                currentTemp.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
-                currentTempBox.appendChild(currentTemp);
-            
-                const minMaxTemperatures = document.createElement("div");
-                minMaxTemperatures.classList.add("details");
-                contentBox.appendChild(minMaxTemperatures);
-            
-                const minMaxTempHeader = document.createElement("h3");
-                minMaxTempHeader.innerHTML = "More:"
-                minMaxTemperatures.appendChild(minMaxTempHeader);
-            
-                const minTemp = document.createElement("span");
-                minTemp.classList.add("min-temp")
-                minTemp.innerHTML = data.forecast.forecastday[i].day.mintemp_c  + "°C";
-                minMaxTemperatures.appendChild(minTemp);
-            
-                const maxTemp = document.createElement("span");
-                maxTemp.classList.add("max-temp")
-                maxTemp.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
-                minMaxTemperatures.appendChild(maxTemp);
-            }
+            createCards(cityName);
+            clearContainer();
 });
 
-//anais
+//anais fetch data
 async function fetchApiData(cityName) {
     const weatherApi = "http://api.weatherapi.com/v1/forecast.json?key=" + API.key + "&q=" + encodeURIComponent(cityName) + "&days=7&aqi=no&alerts=no";
     try {
@@ -199,6 +130,7 @@ async function fetchApiData(cityName) {
 }
 
 
+//anais clear container
 function clearContainer() {
     const container = document.querySelector(".container");
     while (container.lastChild) {
@@ -206,4 +138,78 @@ function clearContainer() {
 }
 }
 
+//anais create card
+async function createCards(cityName) {
+    const data = await fetchApiData(cityName);
+    console.log(data);
+    for(let i= 0; i < 5; i++) {
+        const container = document.querySelector('.container');
 
+        const date = new Date()
+        const dayOfTheWeek = weekdays[(date.getDay() + i) % 7]
+    
+        // Create the elements with Data
+        const card = document.createElement('div');
+        card.classList.add("card");
+    
+        if (i === 0) card.classList.add("main-card");
+    
+        container.appendChild(card);
+    
+        const imageBox = document.createElement('div');
+        imageBox.classList.add("imgBx");
+        card.appendChild(imageBox);
+    
+        const cardImg = document.createElement('img');
+        cardImg.src = data.forecast.forecastday[i].day.condition.icon;
+        imageBox.appendChild(cardImg);
+        
+        const contentBox = document.createElement("div");
+        contentBox.classList.add("contentBx");
+        card.appendChild(contentBox);
+    
+        const cardHeader = document.createElement("h2");
+        cardHeader.innerHTML = dayOfTheWeek;
+        contentBox.appendChild(cardHeader);
+    
+        console.log(data.forecast.forecastday[i].day.condition.text);
+        const tempDescription = document.createElement("h4");
+        tempDescription.innerHTML = data.forecast.forecastday[i].day.condition.text;
+        contentBox.appendChild(tempDescription);
+    
+        const currentTempBox = document.createElement("div");
+        currentTempBox.classList.add("color");
+        contentBox.appendChild(currentTempBox)
+    
+        const currentTempHeader = document.createElement("h3");
+        currentTempHeader.innerHTML = "Temp:"
+        currentTempBox.appendChild(currentTempHeader);
+    
+        const currentTemp = document.createElement("span");
+        currentTemp.classList.add("current-temp");
+
+        // OLD structure from different API
+        // let averageTemp = (result.daily.temperature_2m_min[i] + result.daily.temperature_2m_max[i]) / 2;
+        // if(i === 0) averageTemp = result.current.temperature_2m;
+        currentTemp.innerHTML = data.forecast.forecastday[i].day.avgtemp_c + "°C";
+        currentTempBox.appendChild(currentTemp);
+    
+        const minMaxTemperatures = document.createElement("div");
+        minMaxTemperatures.classList.add("details");
+        contentBox.appendChild(minMaxTemperatures);
+    
+        const minMaxTempHeader = document.createElement("h3");
+        minMaxTempHeader.innerHTML = "More:"
+        minMaxTemperatures.appendChild(minMaxTempHeader);
+    
+        const minTemp = document.createElement("span");
+        minTemp.classList.add("min-temp")
+        minTemp.innerHTML = data.forecast.forecastday[i].day.mintemp_c  + "°C";
+        minMaxTemperatures.appendChild(minTemp);
+    
+        const maxTemp = document.createElement("span");
+        maxTemp.classList.add("max-temp")
+        maxTemp.innerHTML = data.forecast.forecastday[i].day.maxtemp_c + "°C";
+        minMaxTemperatures.appendChild(maxTemp);
+    }
+}
